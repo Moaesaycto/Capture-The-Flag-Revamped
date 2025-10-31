a94b283c50332b60cb8ee280dab250939dd349fb
Moaesaycto <stephen.lerantges.los@gmail.com>
2025-10-31 15:09:35 +1100

    Initial commit

---
+++ CTPBackend/src/main/java/moae/dev/App.java
@@ -0,0 +1,26 @@
+package moae.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class App {
+  @GetMapping("/")
+  public Map<String, String> health() {
+    return Map.of("message", "success");
+  }
+
+  @GetMapping("/status")
+  public Map<String, Object> status() {
+      return Map.of("Players", "test");
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+}
